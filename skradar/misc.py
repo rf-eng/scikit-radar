2094c20de7df6abb0059aec31183850f3dcf8366
Reinhard Feger <47209718+rf-eng@users.noreply.github.com>
2022-06-01 16:37:17 +0200

    misc.py: Initial commit

---
+++ skradar/misc.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def nextpow2(N: int):
+    """
+    Returns the smallest integer x such that 2**x >= N
+
+    Parameters
+    ----------
+    N : int
+        Input value that should be expressed as a power of two using an integer
+        as exponent.
+
+    Returns
+    -------
+    x: int
+        Smallest integer x such that 2**x >= N.
+
+    """
+    return int(np.ceil(np.log2(N)))
