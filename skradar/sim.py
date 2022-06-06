ed7f200f81e5c079328601004ea6cf206c34855d
Reinhard Feger <47209718+rf-eng@users.noreply.github.com>
2022-06-06 10:42:43 +0200

    sim.py: Fix type hint for sim_FMCW_if

--- skradar/sim.py
+++ skradar/sim.py
@@ -4,7 +4,7 @@ from scipy.constants import speed_of_light as c0
 
 def sim_FMCW_if(r: float, B: float, fc: float, N: float, Ts: float,
                 v: float = 0, c: float = c0,
-                cplx: bool = False) -> tuple[np.ndarray, np.ndarray]:
+                cplx: bool = False) -> np.ndarray:
     """
     This function simulates the intermediate frequency (IF) signal of a
     single-chirp of an FMCW radar.
