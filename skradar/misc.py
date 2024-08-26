7790f0937b1507949ee197af5bbdee76ddede55c
Reinhard Feger <47209718+rf-eng@users.noreply.github.com>
2024-08-26 16:46:08 +0200

    misc.py: Add function to convert from dB to linear

--- skradar/misc.py
+++ skradar/misc.py
@@ -18,3 +18,33 @@ def nextpow2(N: float) -> int:
 
     """
     return int(np.ceil(np.log2(N)))
+
+def dBm2W(val_dBm: np.ndarray) -> np.ndarray:
+    """Converts values from dBm to Watt
+
+    Parameters
+    ----------
+    val_dbm : float
+        Input value(s) in dBm
+
+    Returns
+    -------
+    float
+        Output value(s) in Watt
+    """
+    return 10**(val_dBm/10)*1e-3
+
+def dB2lin(val_dB: np.ndarray) -> np.ndarray:
+    """Converts values from dB to linear
+
+    Parameters
+    ----------
+    val_dB : float
+        Input value(s) in dB
+
+    Returns
+    -------
+    float
+        Output value(s) in linear scale
+    """
+    return 10**(val_dB/10)
